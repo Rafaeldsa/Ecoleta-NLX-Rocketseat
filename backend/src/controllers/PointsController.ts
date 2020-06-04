ed02cf991d159bb6315d346e74aa4479440e9985
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2020-06-04 11:07:47 -0300

    .

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -2,6 +2,23 @@ import { Request, Response } from "express";
 import knex from "../database/connection";
 
 class PointsController {
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(",")
+      .map((item) => Number(item.trim()));
+
+    const points = await knex("points")
+      .join("point_items", "points.id", "=", "point_items.point_id")
+      .whereIn("point_items.item_id", parsedItems)
+      .where("city", String(city))
+      .where("uf", String(uf))
+      .distinct()
+      .select("points.*");
+
+    return response.json(points);
+  }
   async show(request: Request, response: Response) {
     const { id } = request.params;
 
@@ -11,7 +28,12 @@ class PointsController {
       return response.status(400).json({ message: "Point not found" });
     }
 
-    return response.json(point);
+    const items = await knex("items")
+      .join("point_items", "items.id", "=", "point_items.item_id")
+      .where("point_items.point_id", id)
+      .select("items.title");
+
+    return response.json({ point, items });
   }
   async create(request: Request, response: Response) {
     const {
